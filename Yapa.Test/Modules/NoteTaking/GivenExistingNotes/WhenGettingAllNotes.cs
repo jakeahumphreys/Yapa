b80036448a231957e8bc44a1b1bc284c7c9d76bb
jakeahumphreys <jakehumphreys@outlook.com>
2025-01-09 10:15:51 +0000

    Get/GetAll note tests

---
+++ Yapa.Test/Modules/NoteTaking/GivenExistingNotes/WhenGettingAllNotes.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Extensions.Time.Testing;
+using Moq;
+using Yapa.Modules.NoteTaking;
+using Yapa.Modules.NoteTaking.Types;
+
+namespace Yapa.Test.Modules.NoteTaking.GivenExistingNotes;
+
+[TestFixture]
+[Parallelizable]
+public sealed class WhenGettingAllNotes
+{
+    private Mock<INoteRepository> _noteRepository;
+    private FakeTimeProvider _timeProvider;
+    private IList<Note> _result;
+
+    [OneTimeSetUp]
+    public async Task Setup()
+    {
+        _noteRepository = new Mock<INoteRepository>();
+        _timeProvider = new FakeTimeProvider(DateTime.UtcNow);
+
+
+        _noteRepository.Setup(x => x.GetAll()).ReturnsAsync(new List<Note>
+        {
+            new Note
+            {
+                Id = Guid.Parse("7e93eba1-8c6b-4534-bbcc-227462a6d3df"),
+                Collection = null,
+                Content = "Test Content",
+                Title = "Note 1",
+                IsArchived = false,
+                ModifiedOn = _timeProvider.GetUtcNow().DateTime,
+                CreatedOn = _timeProvider.GetUtcNow().DateTime,
+            },
+            new Note
+            {
+                Id = Guid.Parse("15b1f591-bf92-4a8f-bbce-0f336d5b2492"),
+                Collection = null,
+                Content = "Test Content",
+                Title = "Note 2",
+                IsArchived = false,
+                ModifiedOn = _timeProvider.GetUtcNow().DateTime,
+                CreatedOn = _timeProvider.GetUtcNow().DateTime,
+            }
+        });
+        
+        var subject = new NoteService(_noteRepository.Object, _timeProvider);
+        _result = await subject.GetAllNotes();
+    }
+    
+    [Test]
+    public void ThenTheExpectedNumberOfNotesIsReturned()
+    {
+        Assert.That(_result, Has.Count.EqualTo(2));
+    }
+
+    [Test]
+    public void ThenTheFirstNoteIsReturned()
+    {
+        var note = _result.First();
+        
+        Assert.That(note.Content, Is.EqualTo("Test Content"));
+        Assert.That(note.Title, Is.EqualTo("Note 1"));
+        Assert.That(note.CreatedOn, Is.EqualTo(_timeProvider.GetUtcNow().DateTime));
+        Assert.That(note.ModifiedOn, Is.EqualTo(_timeProvider.GetUtcNow().DateTime));
+        Assert.That(note.IsArchived, Is.False);
+        Assert.That(note.Id, Is.EqualTo(Guid.Parse("7e93eba1-8c6b-4534-bbcc-227462a6d3df")));
+        Assert.That(note.Collection, Is.Null);
+    }
+    
+    [Test]
+    public void ThenTheSecondNoteIsReturned()
+    {
+        var note = _result.Last();
+        
+        Assert.That(note.Content, Is.EqualTo("Test Content"));
+        Assert.That(note.Title, Is.EqualTo("Note 2"));
+        Assert.That(note.CreatedOn, Is.EqualTo(_timeProvider.GetUtcNow().DateTime));
+        Assert.That(note.ModifiedOn, Is.EqualTo(_timeProvider.GetUtcNow().DateTime));
+        Assert.That(note.IsArchived, Is.False);
+        Assert.That(note.Id, Is.EqualTo(Guid.Parse("15b1f591-bf92-4a8f-bbce-0f336d5b2492")));
+        Assert.That(note.Collection, Is.Null);
+    }
+}
---
+++ Yapa.Test/Modules/NoteTaking/GivenExistingNotes/WhenGettingById.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Time.Testing;
+using Moq;
+using Yapa.Modules.NoteTaking;
+using Yapa.Modules.NoteTaking.Types;
+
+namespace Yapa.Test.Modules.NoteTaking.GivenExistingNotes;
+
+[TestFixture]
+[Parallelizable]
+public sealed class WhenGettingById
+{
+    private Mock<INoteRepository> _noteRepository;
+    private FakeTimeProvider _timeProvider;
+    private Guid _noteId;
+    private Note _result;
+
+    [OneTimeSetUp]
+    public async Task Setup()
+    {
+        _noteRepository = new Mock<INoteRepository>();
+        _noteId = Guid.Parse("a83d6396-9a27-4ce9-a488-a3cc8c181ab5");
+        _timeProvider = new FakeTimeProvider(DateTime.UtcNow);
+
+        _noteRepository.Setup(x => x.GetById(It.Is<Guid>(y => y == _noteId))).ReturnsAsync(new Note
+        {
+            Id = _noteId,
+            Collection = null,
+            Content = "Test Content",
+            Title = "Test Title",
+            IsArchived = false,
+            ModifiedOn = _timeProvider.GetUtcNow().DateTime,
+            CreatedOn = _timeProvider.GetUtcNow().DateTime,
+        });
+        
+        var subject = new NoteService(_noteRepository.Object, _timeProvider);
+        _result = await subject.GetNoteById(_noteId);
+    }
+    
+    [Test]
+    public void ThenANoteIsReturned()
+    {
+        Assert.That(_result, Is.Not.Null);
+    }
+
+    [Test]
+    public void ThenTheExpectedNoteIsReturned()
+    {
+        Assert.That(_result.Content, Is.EqualTo("Test Content"));
+        Assert.That(_result.Title, Is.EqualTo("Test Title"));
+        Assert.That(_result.CreatedOn, Is.EqualTo(_timeProvider.GetUtcNow().DateTime));
+        Assert.That(_result.ModifiedOn, Is.EqualTo(_timeProvider.GetUtcNow().DateTime));
+        Assert.That(_result.IsArchived, Is.False);
+        Assert.That(_result.Id, Is.EqualTo(_noteId));
+        Assert.That(_result.Collection, Is.Null);
+    }
+}
