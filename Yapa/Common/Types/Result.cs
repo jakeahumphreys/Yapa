bbc74bf2c67fa0fd461ad9ed951e7a75b9fef0fc
jakeahumphreys <jakehumphreys@outlook.com>
2025-01-09 21:44:53 +0000

    Encapsulate results in an object with generic type content, Can be used to serve errors neatly to the FE

---
+++ Yapa/Common/Types/Result.cs
@@ -0,0 +1,17 @@
+﻿namespace Yapa.Common.Types;
+
+public class Result<T>
+{
+    public T Content { get;}
+    public string ErrorMessage { get;}
+    public bool HasError => !string.IsNullOrWhiteSpace(ErrorMessage);
+
+    private Result(T content, string errorMessage)
+    {
+        ErrorMessage = errorMessage;
+        Content = content;
+    }
+    
+    public static Result<T> Success(T content) => new Result<T>(content, string.Empty);
+    public static Result<T> Failure (string errorMessage) => new Result<T>(default, errorMessage);
+}
