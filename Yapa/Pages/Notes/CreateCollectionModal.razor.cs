ff70f7793ec7b7a903892d96dae7f7159167a6c3
jakeahumphreys <jakehumphreys@outlook.com>
2025-01-10 12:24:51 +0000

    A new modal for adding collections

---
+++ Yapa/Pages/Notes/CreateCollectionModal.razor.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Yapa.Features.NoteTaking;
+
+namespace Yapa.Pages.Notes;
+
+public partial class CreateCollectionModal : ComponentBase
+{
+    [CascadingParameter] MudDialogInstance MudDialog { get; set; }
+    [Parameter] public EventCallback<bool> OnDialogClose { get; set; }
+    
+    [Inject] private CollectionService CollectionService { get; set; }
+    
+    private string _collectionName;
+    private string _errorMessage;
+
+    private async Task AddCollection()
+    {
+        var result = await CollectionService.AddCollection(_collectionName);
+        
+        if(result.HasError)
+            _errorMessage = result.ErrorMessage;
+        else
+            MudDialog.Close(DialogResult.Ok(true));
+    }
+
+    private void CloseDialog(bool result)
+    {
+        OnDialogClose.InvokeAsync(result);
+        MudDialog.Close();
+    }
+}
