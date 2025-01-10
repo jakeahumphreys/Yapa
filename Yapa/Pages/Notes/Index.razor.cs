f0357c411a7882c886589a82ffe0ac89653db149
jakeahumphreys <jakehumphreys@outlook.com>
2025-01-10 10:43:43 +0000

    Starting to flesh out the notes FE

---
+++ Yapa/Pages/Notes/Index.razor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Yapa.Features.NoteTaking;
+using Yapa.Features.NoteTaking.Types;
+
+namespace Yapa.Pages.Notes;
+
+public partial class Index : ComponentBase
+{
+    [Inject]
+    public CollectionService CollectionService { get; set; }
+    
+    private List<CollectionRecord> Collections { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var storedCollections = await CollectionService.GetAll();
+        
+        Collections = storedCollections.Content;
+        await base.OnInitializedAsync();
+    }
+}
